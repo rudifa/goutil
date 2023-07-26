bc0bad2df98f5404249771d5d4387a05aa96f2ae
Rudi Farkas <rudi.farkas@gmail.com>
2023-07-26 14:41:39 +0200

    add ReadFromFile + tests

--- files/files_test.go
+++ files/files_test.go
@@ -28,3 +28,26 @@ func TestEnsureDirectoryExists(t *testing.T) {
 		t.Errorf("RemoveDirectoryIfExists failed: %v", err)
 	}
 }
+
+// TestWriteToandreadFromFile tests WriteTo and ReadFromFile
+func TestWriteToFileAndReadFromFile(t *testing.T) {
+
+	err := files.WriteToFile("test.txt", "Hello World!")
+	if err != nil {
+		t.Errorf("WriteTo failed: %v", err)
+	}
+
+	content, err := files.ReadFromFile("test.txt")
+	if err != nil {
+		t.Errorf("ReadFromFile failed: %v", err)
+	}
+	if content != "Hello World!" {
+		t.Errorf("ReadFromFile failed: %v", err)
+	}
+
+	// remove test.txt
+	err = files.RemoveFileIfExists("test.txt")
+	if err != nil {
+		t.Errorf("RemoveFileIfExists failed: %v", err)
+	}
+}
